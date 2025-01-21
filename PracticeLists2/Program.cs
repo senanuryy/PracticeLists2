a37641c79157f168410a18821b072bcb5ecfa5a4
Senanur Yavuzarslan <yavuzarslansenanur@gmail.com>
2025-01-21 19:11:04 +0300

    Add project files.

---
+++ PracticeLists2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Channels;
+
+List<string> kahveler = new List<string>();
+
+Console.WriteLine("5 Tane Kahve İsmi Giriniz:");
+
+string kahve;
+
+for (int i = 1; i < 6; i++)
+{
+    Console.Write("Kahve " + i + ": ");
+    kahve = Console.ReadLine();
+    kahveler.Add(kahve);    
+}
+
+Console.WriteLine("\nGirilen Kahve İsimleri:");
+foreach (string item in kahveler)
+{
+    Console.WriteLine(item);
+}
